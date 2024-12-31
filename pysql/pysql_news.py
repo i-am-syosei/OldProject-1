4f9edf09365f38e883aa0ffcecfa1d5b0710985e
Syosei <104332418+i-am-syosei@users.noreply.github.com>
2024-12-31 17:10:14 +0900

    Add files via upload

---
+++ pysql/pysql_news.py
@@ -0,0 +1,75 @@
+import mysql.connector
+import datetime
+
+print("Content-Type: text/html\n")
+from random import sample
+import pandas as pd
+print(pd.__version__)
+from pytrends.request import TrendReq
+
+dt = datetime.datetime.today()
+print(dt)
+pytrends = TrendReq(hl='en-US', tz=360)
+japan = pytrends.trending_searches("japan")
+news = japan.values
+
+
+cnx = None
+try:
+    cnx = mysql.connector.connect(
+        user='',  # ユーザー名
+        password='',  # パスワード
+        host='',  # ホスト名(IPアドレス）
+        database=''  # データベース名
+    )
+    zero = 0
+    cursor = cnx.cursor()
+    
+    
+    for num in range(10):
+        
+        sql = ('''
+               
+        INSERT INTO news(title_id, title_name, title_day, count) 
+        SELECT %s,%s,%s,%s
+        WHERE NOT EXISTS (SELECT * FROM news WHERE title_name = %s);      
+        
+        ''')
+        
+        data = [
+        ('',str(*news[num]),dt,zero,str(*news[num])),
+        ]
+        cursor.executemany(sql, data)
+        
+        #同じタイトルがある場合時間を変える。更新時にcount+1
+        sql = ('''
+        UPDATE  news
+        SET     title_day = %s,count = count + 1
+        WHERE   title_name = %s
+        ''')
+        param = [
+        (dt,str(*news[num])),
+        ]
+        cursor.executemany(sql, param)
+        
+        #ランキング更新
+        sql = ('''
+        UPDATE  rank
+        SET     rank_title = %s
+        WHERE   rank_id = %s
+        ''')
+        
+        data1 = [
+        
+        (str(*news[num]),num+1),
+    ]
+        cursor.executemany(sql, data1)
+        cnx.commit()
+    cursor.close()
+
+except Exception as e:
+    print(f"Error Occurred: {e}")
+
+finally:
+    if cnx is not None and cnx.is_connected():
+        cnx.close()
---
+++ pysql/pysql_news_US.py
@@ -0,0 +1,84 @@
+import mysql.connector
+import datetime
+
+print("Content-Type: text/html\n")
+from random import sample
+import pandas as pd
+print(pd.__version__)
+from pytrends.request import TrendReq
+
+dt = datetime.datetime.today()
+print(dt)
+#Googleに接続する
+pytrends = TrendReq(hl='en-US', tz=360)
+#毎日の検索トレンドを取得
+US = pytrends.trending_searches(pn='united_states')
+#最初の5行を表示する
+news = US.values
+
+
+cnx = None
+try:
+    cnx = mysql.connector.connect(
+        user='',  # ユーザー名
+        password='',  # パスワード
+        host='',  # ホスト名(IPアドレス）
+        database=''  # データベース名
+    )
+    zero = 0
+    cursor = cnx.cursor()
+    
+    
+    for num in range(10):
+        
+    
+        #新たなタイトルを入れる
+        sql = ('''
+               
+        INSERT INTO news_us(title_id, title_name, title_day, count) 
+        SELECT %s,%s,%s,%s
+        WHERE NOT EXISTS (SELECT * FROM news_us WHERE title_name = %s);      
+        
+        ''')
+        
+        data = [
+        ('',str(*news[num]),dt,zero,str(*news[num])),
+        ]
+        cursor.executemany(sql, data)  
+        
+        #同じタイトルがある場合時間を変える。更新時にcount+1
+        sql = ('''
+        UPDATE  news_us
+        SET     title_day = %s,count = count + 1
+        WHERE   title_name = %s
+        ''')
+        param = [
+        (dt,str(*news[num])),
+        ]
+        cursor.executemany(sql, param)
+        
+        #ランキング更新
+        sql = ('''
+        UPDATE  rank_us
+        SET     rank_title = %s
+        WHERE   rank_id = %s;
+        
+        ''')
+        
+        data1 = [
+        
+        (str(*news[num]),num+1),
+    ]
+        cursor.executemany(sql, data1)
+        
+        
+        cnx.commit()
+
+    cursor.close()
+
+except Exception as e:
+    print(f"Error Occurred: {e}")
+
+finally:
+    if cnx is not None and cnx.is_connected():
+        cnx.close()
